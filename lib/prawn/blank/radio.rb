2b384380437ec120de596561b512cd2389d75986
Reginald Tan <redge.tan@gmail.com>
2019-10-03 15:19:10 -0400

    dont force radio button export value to be index based. use previous export value

--- lib/prawn/blank/radio.rb
+++ lib/prawn/blank/radio.rb
@@ -9,19 +9,18 @@ module Prawn::Blank
 
       app = appearance || document.default_appearance
 
-      radio_index = (parent.children.length - 1).to_s
-
       @data[:AP] =
         if check_string
-          { N: { :Off => app.checkbox_off(self), radio_index => app.checkbox_on(self) },
-            R: { :Off => app.checkbox_off_over(self), radio_index => app.checkbox_on_over(self) },
-            D: { :Off => app.checkbox_off_down(self), radio_index => app.checkbox_on_down(self) } }
+          { N: { :Off => app.checkbox_off(self), @value => app.checkbox_on(self) },
+            R: { :Off => app.checkbox_off_over(self), @value => app.checkbox_on_over(self) },
+            D: { :Off => app.checkbox_off_down(self), @value => app.checkbox_on_down(self) } }
         else
-          { N: { :Off => app.radio_off(self), radio_index => app.radio_on(self) },
-            R: { :Off => app.radio_off_over(self), radio_index => app.radio_on_over(self) },
-            D: { :Off => app.radio_off_down(self), radio_index => app.radio_on_down(self) } }
+          { N: { :Off => app.radio_off(self), @value => app.radio_on(self) },
+            R: { :Off => app.radio_off_over(self), @value => app.radio_on_over(self) },
+            D: { :Off => app.radio_off_down(self), @value => app.radio_on_down(self) } }
         end
-      @data[:AS] = radio_index === @value ? radio_index.to_sym : :Off
+
+      @data[:AS] = parent.value == @value ? @value : :Off
 
       if check_string
         # right now, Adobe is replacing our appearance AP content stream with its own defaults
